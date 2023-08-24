b387f9a3bfbbdc2d3d83d6ae022a362167140bf2
olucassquadross <olucasquadros@gmail.com>
2023-08-23 21:19:11 -0300

    first commit

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,22 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Cart from "./components/Cart";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { CartProvider } from "./components/CartContext";
 import ProductList from "./components/ProductList";
 import ProductDetail from "./components/ProductDetail";
-import './App.css';
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
 
 function App() {
   return (
     <Router>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/cart">Carrinho</Link>
-      </nav>
-      <Routes>
-        <Route index element={<ProductList />} />
-        <Route path="products/:id" element={<ProductDetail />} />
-        <Route path="cart" element={<Cart />} />
-      </Routes>
+      <CartProvider>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Página Inicial</Link>
+            </li>
+            <li>
+              <Link to="/cart">Carrinho</Link>
+            </li>
+            <li>
+              <Link to="/checkout">Finalizar Compra</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </CartProvider>
     </Router>
   );
 }
